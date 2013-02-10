1b5e35494b9e0462c8c6feddfe02b73e9629c452
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2013-02-10 01:06:44 +0100

    [Boost] Enriched the query status Shell script.

--- mypackages/boost/query_status.sh
+++ mypackages/boost/query_status.sh
@@ -9,7 +9,8 @@ fi
 
 # Logging
 LOG_FILE=boost_deplist_pack_task_allinfo.txt
-rm -f boost_deplist_pack_task_all_success.txt boost_deplist_pack_task_all_failure.txt boost_deplist_pack_task_all_unfinished.txt
+rm -f ${LOGFILE} boost_deplist_pack_task_all_success.txt \
+ boost_deplist_pack_task_all_failure.txt boost_deplist_pack_task_all_unfinished.txt
 
 #
 BOOST_DEP_TASKLIST=`cat boost_deplist_pack_task_all.txt`
@@ -18,20 +19,24 @@ do
 	echo "Querying the Koji task #${_task}..."
 	KOJI_STATUS=`koji taskinfo ${_task}`
 	echo "${KOJI_STATUS}" >> ${LOG_FILE}
+	echo >> ${LOG_FILE}
 	unset KOJI_FAILED
 	echo "${KOJI_STATUS}" | grep "State: failed" && KOJI_FAILED=${_task}
 	KOJI_PACK=`echo "${KOJI_STATUS}" | grep "Build: " | cut -d':' -f2 | cut -d' ' -f2`
 	if [ -n "${KOJI_FAILED}" ]
 	then
 		echo "The Koji task #${_task} (http://koji.fedoraproject.org/koji/taskinfo?taskID=${_task}) for ${KOJI_PACK} failed." | tee -a boost_deplist_pack_task_all_failure.txt
+		echo >> boost_deplist_pack_task_all_failure.txt
 	else
 		unset KOJI_CLOSED
-		echo "${KOJI_STATUS}" | grep "State: failed" && KOJI_CLOSED=${_task}
+		echo "${KOJI_STATUS}" | grep "State: closed" && KOJI_CLOSED=${_task}
 		if [ -n "${KOJI_CLOSED}" ]
 		then
 			echo "The Koji task #${_task} (http://koji.fedoraproject.org/koji/taskinfo?taskID=${_task}) for ${KOJI_PACK} succeeded." | tee -a boost_deplist_pack_task_all_success.txt
+			echo >> boost_deplist_pack_task_all_success.txt
 		else
 			echo "The Koji task #${_task} (http://koji.fedoraproject.org/koji/taskinfo?taskID=${_task}) for ${KOJI_PACK} has not finished yet." | tee -a boost_deplist_pack_task_all_unfinished.txt
+			echo >> boost_deplist_pack_task_all_unfinished.txt
 		fi
 	fi
 done
